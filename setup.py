becfd094f1a146a2bcb5035b0baf20a99a02e8c7
Florian Matter <florianmatter@gmail.com>
2022-05-30 12:37:19 -0700

    black

--- setup.py
+++ setup.py
@@ -2,45 +2,46 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='yawarana_grammar',
-    version='0.0.1.dev',
-    description='yawarana_grammar',
+    name="yawarana_grammar",
+    version="0.0.1.dev",
+    description="yawarana_grammar",
     classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
     ],
-    author='Florian Matter',
-    author_email='florianmatter@gmail.com',
-    url='https://github.com/fmatter/yawarana-sketch-clld',
-    keywords='web pyramid pylons',
+    author="Florian Matter",
+    author_email="florianmatter@gmail.com",
+    url="https://github.com/fmatter/yawarana-sketch-clld",
+    keywords="web pyramid pylons",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'clld',  # >=7.0
-        'waitress',
-        'clld_morphology_plugin @ git+https://git@github.com/fmatter/clld-morphology-plugin',
-        'clld_corpus_plugin @ git+https://git@github.com/fmatter/clld-corpus-plugin',
-        'clld_markdown_plugin @ git+https://git@github.com/clld/clld-markdown-plugin',
-        'cldfviz'
-],
-extras_require={
-        'dev': ['flake8', 'waitress'],
-        'test': [
-            'mock',
-            'pytest>=5.4',
-            'pytest-clld',
-            'pytest-mock',
-            'pytest-cov',
-            'coverage>=4.2',
-            'selenium',
-            'zope.component>=3.11.0',
+        "clld",  # >=7.0
+        "waitress",
+        "clld_morphology_plugin @ git+https://git@github.com/fmatter/clld-morphology-plugin",
+        "clld_corpus_plugin @ git+https://git@github.com/fmatter/clld-corpus-plugin",
+        "clld_markdown_plugin @ git+https://git@github.com/clld/clld-markdown-plugin",
+        "cldfviz",
+    ],
+    extras_require={
+        "dev": ["flake8", "waitress"],
+        "test": [
+            "mock",
+            "pytest>=5.4",
+            "pytest-clld",
+            "pytest-mock",
+            "pytest-cov",
+            "coverage>=4.2",
+            "selenium",
+            "zope.component>=3.11.0",
         ],
     },
     test_suite="yawarana_grammar",
     entry_points="""\
     [paste.app_factory]
     main = yawarana_grammar:main
-""")
+""",
+)
