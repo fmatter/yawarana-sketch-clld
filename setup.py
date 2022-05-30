20dbb3d55e2e2c4b1db326a7a0262b7f2644acd9
Florian Matter <florianmatter@gmail.com>
2022-05-30 12:47:14 -0700

    0.0.1 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="yawarana_grammar",
-    version="0.0.1.dev",
+    version="0.0.1",
     description="yawarana_grammar",
     classifiers=[
         "Programming Language :: Python",
