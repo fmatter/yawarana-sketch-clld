6e6606f64ba05321b5c89387bfd344336baa956b
Florian Matter <florianmatter@gmail.com>
2022-07-02 11:46:11 -0700

    linting

--- clld_own_markdown.py
+++ clld_own_markdown.py
@@ -7,40 +7,68 @@ from clld.web.util.htmllib import HTML
 from yawarana_sketch_clld.models import Phoneme
 from clld_document_plugin.models import Document
 
+
 def my_render_ex(req, objid, ids=None, subexample=False, **kwargs):
     if "subexample" in kwargs.get("format", []):
-        subexample=True
+        subexample = True
     example_id = kwargs.get("example_id", [None])[0]
     if objid == "__all__":
         if ids:
-            ex_strs = [my_render_ex(req, mid, subexample=True) for mid in ids[0].split(",")]
-            return HTML.ol(HTML.li(HTML.ol(*ex_strs, class_="subexample"), class_="example", id_=example_id), class_="example")
+            ex_strs = [
+                my_render_ex(req, mid, subexample=True) for mid in ids[0].split(",")
+            ]
+            return HTML.ol(
+                HTML.li(
+                    HTML.ol(*ex_strs, class_="subexample"),
+                    class_="example",
+                    id_=example_id,
+                ),
+                class_="example",
+            )
     sentence = DBSession.query(Sentence).filter(Sentence.id == objid).first()
     if subexample:
-        return rendered_sentence(req, sentence, sentence_link=True, counter_class="subexample", in_context=True)
+        return rendered_sentence(
+            req,
+            sentence,
+            sentence_link=True,
+            counter_class="subexample",
+            in_context=True,
+        )
     else:
-        return HTML.ol(rendered_sentence(req, sentence, sentence_link=True, in_context=True, example_id=example_id, counter_class="example"), class_="example")
-    return 
+        return HTML.ol(
+            rendered_sentence(
+                req,
+                sentence,
+                sentence_link=True,
+                in_context=True,
+                example_id=example_id,
+                counter_class="example",
+            ),
+            class_="example",
+        )
+    return
 
-def render_morpheme(req, objid, **kwargs):
-    morpheme = DBSession.query(Morpheme).filter(Morpheme.id == objid).first()
-    url = req.route_url("morpheme", id=objid, **kwargs)
-    with_translation = "with_translation" in kwargs
-    md_str = f"*[{morpheme.name}]({url})*"
-    if with_translation:
-        meanings = [x.meaning.name for x in morpheme.meanings]
-        md_str += f" ‘{', '.join(meanings)}’"
-    return md_str
+custom_map = {
+    "FormTable": {"route": "wordform", "model": Wordform},
+    "MorphsetTable": {"route": "morpheme", "model": Morpheme},
+    "MorphTable": {"route": "morph", "model": Morph},
+}
 
-def render_wf(req, objid, **kwargs):
-    wf = DBSession.query(Wordform).filter(Wordform.id == objid).first()
-    url = req.route_url("wordform", id=objid, **kwargs)
+
+def render_lfts(req, objid, table, **kwargs):
+    obj = (
+        DBSession.query(custom_map[table]["model"])
+        .filter(custom_map[table]["model"].id == objid)
+        .first()
+    )
+    url = req.route_url(custom_map[table]["route"], id=objid, **kwargs)
     with_translation = "no_translation" not in kwargs
-    md_str = f"*[{wf.name}]({url})*"
+    md_str = f"*[{obj.name}]({url})*"
     if with_translation:
-        meanings = [x.meaning.name for x in wf.meanings]
+        meanings = [x.meaning.name for x in obj.meanings]
         md_str += f" ‘{', '.join(meanings)}’"
     return md_str
+
 
 custom_model_map = {
     "MorphTable": {"route": "morph", "model": Morph, "decorate": lambda x: f"*{x}*"},
@@ -51,8 +79,20 @@ custom_model_map = {
     },
     "TextTable": {"route": "text", "model": Text, "decorate": lambda x: f"'{x}'"},
     "POSTable": {"route": "pos", "model": POS},
-    "FormTable": {"route": "wordform", "model": Wordform, "decorate": lambda x: f"*{x}*"},
+    "FormTable": {
+        "route": "wordform",
+        "model": Wordform,
+        "decorate": lambda x: f"*{x}*",
+    },
     "ChapterTable": {"route": "document", "model": Document},
-    "PhonemeTable": {"route": "phoneme", "model": Phoneme, "decorate": lambda x: f"/{x}/"},
+    "PhonemeTable": {
+        "route": "phoneme",
+        "model": Phoneme,
+        "decorate": lambda x: f"/{x}/",
+    },
 }
-custom_function_map = {"ExampleTable": my_render_ex, "MorphsetTable": render_morpheme, "FormTable": render_wf}
+custom_function_map = {
+    "ExampleTable": my_render_ex,
+    "MorphsetTable": render_lfts,
+    "FormTable": render_lfts,
+}
